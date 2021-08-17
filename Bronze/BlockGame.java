974f9875967e12533ad82b10fea5c8f5fdc6fcb4
Ben <battlefrontgamer555@gmail.com>
2021-08-17 07:06:40 -0400

    Format Block Game

--- Bronze/BlockGame.java
+++ Bronze/BlockGame.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.*;
 
+//USACO 2016 December Contest, Bronze Problem 2
+//Link: http://www.usaco.org/index.php?page=viewproblem2&cpid=664
 public class BlockGame {
     public static void main(String[] args) throws IOException {
         BufferedReader r = new BufferedReader(new FileReader("blocks.in"));
@@ -10,7 +12,7 @@ public class BlockGame {
         int N = Integer.parseInt(st.nextToken());
         int[] lettersNeeded = new int[26];
 
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(r.readLine());
             String firstWord = st.nextToken();
             String secondWord = st.nextToken();
@@ -18,17 +20,16 @@ public class BlockGame {
             int[] firstWordLetters = getLetters(firstWord);
             int[] secondWordLetters = getLetters(secondWord);
 
-            for(int j = 0; j < 26; j++) {
-                if(firstWordLetters[j] > secondWordLetters[j]) {
+            for (int j = 0; j < 26; j++) {
+                if (firstWordLetters[j] > secondWordLetters[j]) {
                     lettersNeeded[j] += firstWordLetters[j];
-                }
-                else {
+                } else {
                     lettersNeeded[j] += secondWordLetters[j];
                 }
             }
         }
 
-        for(int i: lettersNeeded) {
+        for (int i : lettersNeeded) {
             pw.println(i);
         }
         pw.close();
@@ -36,8 +37,8 @@ public class BlockGame {
 
     public static int[] getLetters(String s) {
         int[] lettersNeeded = new int[26];
-        for(int i = 0; i < s.length(); i++) {
-            int index = s.charAt(i)  - 'a';
+        for (int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i) - 'a';
             lettersNeeded[index]++;
         }
         return lettersNeeded;
