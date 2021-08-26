4dc44ec1c8de959625b3372cea68ba61b67b4b9e
Ben <battlefrontgamer555@gmail.com>
2021-08-26 11:28:55 -0400

    Move around Bronze problems and add Milk Measurement

---
+++ USACO/Bronze/MilkMeasurement.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class MilkMeasurement {
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new FileReader("measurement.in"));
+        PrintWriter pw = new PrintWriter(new FileWriter("measurement.out"));
+
+        int n = Integer.parseInt(r.readLine());
+        int[] day = new int[n];
+        String[] name = new String[n];
+        int[] change = new int[n];
+
+        int mildredMilk = 7;
+        int elsieMilk = 7;
+        int bessieMilk = 7;
+
+        boolean mildredTop = true;
+        boolean elsieTop = true;
+        boolean bessieTop = true;
+
+        int boardChange = 0;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(r.readLine());
+            day[i] = Integer.parseInt(st.nextToken());
+            name[i] = st.nextToken();
+            change[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int today = 1; today <= 100; today++) {
+            for (int i = 0; i < n; i++) {
+                if (day[i] == today) {
+                    if (name[i].equals("Mildred")) {
+                        mildredMilk += change[i];
+                    } else if (name[i].equals("Bessie")) {
+                        bessieMilk += change[i];
+                    } else {
+                        elsieMilk += change[i];
+                    }
+                }
+            }
+
+            int topCow = Math.max(mildredMilk, Math.max(bessieMilk, elsieMilk));
+
+            boolean currMildredTop = mildredMilk == topCow;
+            boolean currElsieTop = elsieMilk == topCow;
+            boolean currBessieTop = bessieMilk == topCow;
+
+            if (currMildredTop != mildredTop || currElsieTop != elsieTop || currBessieTop != bessieTop) {
+                boardChange++;
+            }
+
+            mildredTop = currMildredTop;
+            bessieTop = currBessieTop;
+            elsieTop = currElsieTop;
+        }
+
+        pw.println(boardChange);
+        pw.close();
+    }
+}
