2cb0a9efb1c5b7c9fea2becbc8ebe2d135982bfd
Ben <battlefrontgamer555@gmail.com>
2021-12-02 15:48:02 -0500

    file reorganization

--- Advent Of Code/Day 2/Day2P2.java
+++ Advent Of Code/Day 2/Day2P2.java
@@ -4,7 +4,7 @@ import java.util.*;
 //Link: https://adventofcode.com/2021/day/2
 public class Day2P2 {
   public static void main(String[] args) throws IOException {
-    BufferedReader r = new BufferedReader(new FileReader("Advent Of Code/commands.txt"));
+    BufferedReader r = new BufferedReader(new FileReader("Advent Of Code/Day 2/commands.txt"));
     StringTokenizer st;
 
     final int K_MEASUREMENT_NUMS = 1000;
